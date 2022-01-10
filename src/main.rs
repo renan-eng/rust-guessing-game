d0fb6ec8efd3d646634a29a64ab55a26c35f4ff0
renan-eng <renan3838@gmail.com>
2022-01-10 18:02:05 -0300

    Criado jogo de adivinhar o numero

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    //Generating a random number between 1 and 100
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    println!("The secret number is: {}", secret_number);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
     
-    println!("You guesses: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {            
+            Ordering::Greater => println!("Too big!!"),
+            Ordering::Less => println!("Too small!!"),
+            Ordering::Equal => {
+                println!("You Win!!");
+                break;
+            }
+        }
+    }
 }
